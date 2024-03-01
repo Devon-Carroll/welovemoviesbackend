ddd5dac4eee15efd48c953e60c2f18721b78393a
Devon <41218582+Devon-Carroll@users.noreply.github.com>
2024-03-01 13:40:57 -0500

    Update reviews.router.js

--- src/reviews/reviews.router.js
+++ src/reviews/reviews.router.js
@@ -1,10 +1,14 @@
-const router = require("express").Router();
-const controller = require("./reviews.controller");
-const methodNotAllowed = require("../errors/methodNotAllowed");
+const router = require("express").Router({ mergeParams: true })
+const controller = require("./reviews.controller")
+const methodNotAllowed = require("../errors/methodNotAllowed")
 
-router.route("/").post(controller.create).all(methodNotAllowed);
 
-router.route("/:reviewsId").get(controller.read).put(controller.update).delete(controller.delete).all(methodNotAllowed);
+// PUT/DELETE (/reviews/:reviewId)
+
+router.route("/:reviewId")
+    .put(controller.update)  // Route for updating a review
+    .delete(controller.delete) // Route for deleting a review 
+    .all(methodNotAllowed)
 
 
 module.exports = router;
